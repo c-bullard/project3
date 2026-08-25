e386fe8e9a724fae54133fbfe6734703bb31a10b
Chad Bullard <clbullard@live.com>
2026-08-25 14:32:52 -0600

    Adds component for decks

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import './styles/App.css';
 import Cards from './components/Cards';
 import CardDetails from './components/CardDetails';
 import Collection from './components/Collection';
+import Decks from './components/Decks';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Cards />} />
           <Route path="/collection" element={<Collection />} />
+          <Route path="/decks" element={<Decks />} />
           <Route path="/test" element={<CardDetails />} />
         </Routes>
       </Router>
---
+++ client/src/components/Decks.jsx
@@ -0,0 +1,63 @@
+import { useEffect, useState } from 'react';
+import '../styles/decks.css';
+
+export default function Decks() {
+  const [decks, setDecks] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      setError(null);
+
+      try {
+        const response = await fetch('http://localhost:8080/decks');
+        if (!response.ok) {
+          throw new Error(`error status: ${response.status}`);
+        }
+        const data = await response.json();
+        setDecks(data);
+        setLoading(false);
+      } catch (err) {
+        setError(err.message);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  if (loading) {
+    return <p className="status-message">Loading decks...</p>;
+  }
+
+  if (error) {
+    return <p className="status-message">Error: {error}</p>;
+  }
+
+  return (
+    <>
+      <div className="deck-list">
+        <table>
+          <caption>My Deck List</caption>
+          <thead>
+            <tr>
+              <th>Deck ID</th>
+              <th>Deck Name</th>
+              <th>Description</th>
+            </tr>
+          </thead>
+          <tbody>
+            {decks.map((deck) => (
+              <tr key={deck.id}>
+                <td>{deck.id}</td>
+                <td>{deck.name}</td>
+                <td>{deck.description}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </>
+  );
+}
